247f32bcffb453ff388b94d20c18427783d2d0a5
maccali <guilherme.maccali@universo.univates.br>
2022-04-27 23:39:29 -0300

    🌍 update stack

--- next.config.js
+++ next.config.js
@@ -1,24 +1,40 @@
-const withPWA = require('next-pwa')
+const withPWA = require("next-pwa");
 const Dotenv = require("dotenv-webpack");
+const runtimeCaching = require("next-pwa/cache");
+const util = require("util");
+
+const cacheTime = 365 * 24 * 60 * 60; // 365 days
+
+runtimeCaching.map((item) => {
+  item.options.expiration.maxAgeSeconds = cacheTime;
+  if (item.options.cacheName === "others") {
+    item.options.expiration.maxEntries = 200;
+  }
+  if (item.options.cacheName === "static-image-assets") {
+    item.options.expiration.maxEntries = 100;
+  }
+});
+
+// console.log('😜 RUNTIME CACHE =>', JSON.stringify(runtimeCaching, null, 4))
+// console.log('😜 RUNTIME CACHE =>', runtimeCaching)
 
 module.exports = withPWA({
   pwa: {
-    dest: 'public',
-    importScripts: [
-      '/worker.js'
-    ]
+    dest: "public",
+    importScripts: ["/worker.js"],
+    runtimeCaching,
   },
   webpack: (config) => {
-
     // Add the new plugin to the existing webpack plugins
     config.plugins.push(new Dotenv({ silent: true }));
 
     return config;
   },
   env: {
-    API_URL: process.env.API_URL,
-    MARVEL_API_PUBLIC_KEY: process.env.MARVEL_API_PUBLIC_KEY,
-    MARVEL_API_PRIVATE_KEY: process.env.MARVEL_API_PRIVATE_KEY,
-    MARVEL_API_TS: process.env.MARVEL_API_TS,
+    NASA_API_KEY: process.env.NASA_API_KEY,
   },
-})
+  images: {
+    deviceSizes: [320, 640, 768, 1024, 1600],
+    domains: ["apod.nasa.gov"],
+  },
+});
