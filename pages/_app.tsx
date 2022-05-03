6ab4b7ce70b62fb67203d06bba50214224e3daf5
maccali <guilherme.maccali@universo.univates.br>
2022-05-02 23:35:39 -0300

    fix: ajust layers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,23 @@
 // import 'tailwindcss/tailwindz.css'
-import "../../public/libs/bootstrap/bootstrap.min.css?v=3";
-import "../../public/custom/css/template.css?v=3";
-import "../../public/custom/css/datepicker.css?v=2";
-import "../../public/custom/css/btns.css?v=3";
+import "./../public/custom/css/template.css?v=3";
+import "./../public/libs/bootstrap/bootstrap.min.css?v=3";
 import 'react-toastify/dist/ReactToastify.minimal.css';
 
 import { AppProps } from "next/app";
-import { AuthProvider } from "contexts/AuthContext";
-import { RealTimeProvider } from "contexts/RealTimeContext";
-import { Toast } from "components/toast";
 
-import Meta from "utils/meta";
-import Offline from "utils/offline";
+
+import Meta from "../components/utils/meta";
+import Nav from "../components/utils/nav";
+// import Offline from "../components/utils/offline";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <AuthProvider>
-        <RealTimeProvider>
-          <Offline />
-          <Meta />
-          <Component {...pageProps} />
-          <Toast />
-        </RealTimeProvider>
-      </AuthProvider>
+
+      <Nav />
+      <Meta />
+      <Component {...pageProps} />
+
     </>
   );
 }
